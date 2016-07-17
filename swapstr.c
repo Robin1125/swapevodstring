16f039effab29ac2febe329920809a7a5afd09a5
Robin1125 <robin.it13@bitsathy.ac.in>
2016-07-17 12:23:12 +0530

    Update swapstr.c

--- swapstr.c
+++ swapstr.c
@@ -1,26 +1,24 @@
-    #include<stdio.h>
-    #include<string.h>
-     
-    int main()
-    {
-    	char a[100];
-    	char t;
-    	int i,l;
-     
-    	gets(a);
-    	l=strlen(a);
-     
-    	for(i=0;(i<l);i+=2){
-    		t=a[i];
-    		a[i]=a[i+1];
-    		a[i+1]=t;
-     
-    		if(a[i+3] == '\0'){
-    			break;
-    		}
-    	}
-     
-    	puts(a);
-     
-    return 0;
-    }
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char a[100];
+	char t;
+	int i,l;
+
+	gets(a);
+	l=strlen(a);
+	
+  if(l>1){
+
+	for(i=0;(i<l);i+=2){
+		t=a[i];
+		a[i]=a[i+1];
+		a[i+1]=t;
+
+		if(a[i+3] == '\0'){
+			break;
+		}
+	}
+  }
